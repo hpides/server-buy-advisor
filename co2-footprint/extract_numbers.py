fee9b63802ff74973da7499aa7e57e34ab6d5ec5
Nils Strassenburg <nils.strassenburg@hpi.de>
2024-07-29 17:56:07 +0200

    enhance parsing

--- co2-footprint/extract_numbers.py
+++ co2-footprint/extract_numbers.py
@@ -5,12 +5,9 @@ import pandas as pd
 
 def read_xls_file(file_path):
     try:
-        # Read the .xls file
         df = pd.read_excel(file_path, engine='xlrd')
 
-        # Display the first few rows of the DataFrame
         print("DataFrame loaded successfully. Here are the first few rows:")
-        # print(df.head())
 
         # Return the DataFrame for further processing if needed
         return df
@@ -23,6 +20,7 @@ def read_xls_file(file_path):
 
 
 def get_field(df, field_name):
+    print(field_name)
     return df.loc[df['Unnamed: 0'] == field_name, 'Unnamed: 1'].values[0]
 
 
@@ -30,19 +28,25 @@ def extract_values(df):
     line_voltage = get_field(df, 'Line Voltage')
     total_max_load_input_power = get_field(df, 'Total Max Load Input Power')
     total_idle_input_power = get_field(df, 'Total Idle Input Power')
-    total_utilization_input_power = get_field(df, 'Total Utilization Input Power')
+    power_hardware_and_cooling = get_field(df, 'Total Utilization Input Power')
+    utilization_input_power_total = get_field(df, 'Utilization Input Power total ')
     total_utilization_carbon_emissions = get_field(df, 'Total Utilization Carbon Emissions')
     total_wattage_estimate = get_field(df, 'Total Wattage Estimate (Hardware + Cooling)')
     cooling_watts = get_field(df, 'Number of cooling watts required for each watt generated  ')
+    utilization = get_field(df, 'Utilization (%)')
+    server_lifecycle = get_field(df, 'Server Lifecycle')
 
     result = {
         'line_voltage': float(line_voltage.replace("VAC", "")),
         'total_max_load_input_power': float(total_max_load_input_power.replace("W", "")),
         'total_idle_input_power': float(total_idle_input_power.replace("W", "")),
-        'total_utilization_input_power': float(total_utilization_input_power.replace("W", "")),
-        'total_utilization_carbon_emissions': float(total_utilization_carbon_emissions.replace("t CO2e", "")),
-        'total_wattage_estimate': float(total_wattage_estimate),
-        'cooling_watts': float(cooling_watts)
+        'power_hardware_and_cooling': float(power_hardware_and_cooling.replace("W", "")),
+        'total_utilization_carbon_emissions': float(total_utilization_carbon_emissions.replace("kg CO2e", "")),
+        'total_wattage_estimate_hardware_and_cooling': float(total_wattage_estimate),
+        'cooling_watts': float(cooling_watts),
+        'utilization': float(utilization),
+        'server_lifecycle': float(server_lifecycle),
+        'power_hardware': float(utilization_input_power_total)
     }
 
     return result
@@ -58,12 +62,25 @@ def parse_list_of_files(root_path, list_of_files):
     return result
 
 
+def list_files(directory):
+    # Get a list of all files and directories in the specified directory
+    all_files = os.listdir(directory)
+
+    # Filter out only the files
+    files = [f for f in all_files if os.path.isfile(os.path.join(directory, f))]
+
+    return files
+
+
 if __name__ == '__main__':
     # NOTE: to parse the files, we currently have to open them once in excel and delete the image, then save again as xls file
-    root_path = '/Users/nils/Downloads/'
-    list_of_files = ['test.xls']
+    root_path = '/Users/nils/Downloads/dl325g10'
+    list_of_files = list_files(root_path)
+    dl325g10_parsed_data = parse_list_of_files(root_path, list_of_files)
+    print(dl325g10_parsed_data)
 
-    parsed_data = parse_list_of_files(root_path, list_of_files)
-
-    print(parsed_data)
+    root_path = '/Users/nils/Downloads/dl325g11'
+    list_of_files = list_files(root_path)
+    dl325g11_parsed_data = parse_list_of_files(root_path, list_of_files)
+    print(dl325g11_parsed_data)
 
