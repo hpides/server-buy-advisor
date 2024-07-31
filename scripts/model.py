e1f1da160842bf3d3aedb76b7c8c451885bfafad
Nils Strassenburg <nils.strassenburg@hpi.de>
2024-07-31 21:18:46 +0200

    wip: numbers with 30% util for germany and sweden

--- scripts/model.py
+++ scripts/model.py
@@ -2,9 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-OPEX_PER_YEAR = {
-    "new_system": 0,
-    "old_system": 0
+OLD_SYSTEM = "old_system"
+NEW_SYSTEM = "new_system"
+
+OPEX_PER_YEAR_SWEDEN_30_PERCENT = {
+    NEW_SYSTEM: 149 / 4,  # 158 kg C02e for 4 years of operation
+    OLD_SYSTEM: 158 / 4  # 158 kg C02e for 4 years of operation
+}
+
+OPEX_PER_YEAR_GERMANY_30_PERCENT = {
+    NEW_SYSTEM: 2154 / 4,  # 158 kg C02e for 4 years of operation
+    OLD_SYSTEM: 2312 / 4  # 158 kg C02e for 4 years of operation
 }
 
 class System:
@@ -50,32 +58,24 @@ class System:
 
         return capex_total
 
-    # def calculate_opex_emissions(self, utilization=1):
-    #     ######## Source of GCI: https://app.electricitymaps.com/zone/DE --> 2023 average for DE
-    #
-    #     energy_consumption_per_year = (self.TDP * 24 * 7 * 52) * utilization  #### kWh per year
-    #     GCI = 0.4  #### kg co2 eq per kWh
-    #
-    #     OPEX = energy_consumption_per_year * GCI  ###### Kg co2 per year
-    #
-    #     return OPEX
-
-    def generate_accumm_projected_opex_emissions(self, time_horizon: int, utilization=1):
-        opex = self.calculate_opex_emissions(utilization)
-        projected_emissions = [i * opex for i in range(1, time_horizon + 1)]
+    def generate_accumm_projected_opex_emissions(self, time_horizon: int, system_id: str):
+        opex_per_year = OPEX_PER_YEAR_SWEDEN_30_PERCENT[system_id]
+        projected_emissions = [i * opex_per_year for i in range(1, time_horizon + 1)]
 
         return np.array(projected_emissions)
 
 
 def generate_systems_comparison(new_system: System, old_system: System, time_horizon: int, utilization=1):
-    new_system_opex = new_system.generate_accumm_projected_opex_emissions(time_horizon, utilization)
+    new_system_opex = new_system.generate_accumm_projected_opex_emissions(time_horizon, system_id=NEW_SYSTEM)
     new_system_capex = new_system.calculate_capex_emissions()
-    old_system_opex = old_system.generate_accumm_projected_opex_emissions(time_horizon, utilization)
+    old_system_opex = old_system.generate_accumm_projected_opex_emissions(time_horizon, system_id=NEW_SYSTEM)
 
     performance_factor = old_system.specint / new_system.specint  ##### --> Assumption: Better performance leads to lower utilization, hence less power draw.
 
     new_system_opex = new_system_opex * performance_factor
-    new_system_opex[0] = new_system_opex[0] + new_system_capex  ###### --> Add the CAPEX at time 0
+
+    # new_system_opex[0] = new_system_opex[0] + new_system_capex  ###### --> Add the CAPEX at time 0
+    new_system_opex = np.array([opex + new_system_capex for opex in new_system_opex])
 
     abs_savings = new_system_opex - old_system_opex
     relative_savings = 1 - (old_system_opex / new_system_opex)
@@ -140,29 +140,21 @@ def create_projections_plot(system_a_projected_emissions, system_b_projected_emi
 ###################################################################################################
 ######################################### PARAMETERS ##############################################
 ###################################################################################################
-time_horizon = 6
+time_horizon = 10
 new_chip = 'Xeon Processor E5-2699 v4'
 old_chip = 'Xeon Processor E5-2699 v3'
 intel_cpus_df = pd.read_csv('../intel_cpus_filtered-extended.csv')
 subset = intel_cpus_df.loc[intel_cpus_df['name'].isin([new_chip, old_chip]), ['name', 'TDP', 'package-area-cm2']]
 
-###### New Chip
-
-new_TDP = subset.loc[intel_cpus_df['name'] == new_chip, 'TDP'].values[0]
-###### Old Chip
-
-old_TDP = subset.loc[intel_cpus_df['name'] == old_chip, 'TDP'].values[0]
-
 ##### New Hardware
 # Die size from: https://www.techpowerup.com/cpu-specs/epyc-9334.c2922
 # 4 * 72 mm^2
 new_die_size = 4 * 72 / 100  # cm^2
-new_TDP =
 new_system = System(
     die_size=new_die_size,
-    TDP=new_TDP,
-    specint=1.3,
-    lifetime=5,
+    TDP=None,
+    specint=2,
+    lifetime=10,
     dram_capacity=8 * 64,
     ssd_capacity=8 * 1600,
     hdd_capacity=0
@@ -174,9 +166,9 @@ new_system = System(
 old_die_size = 74 / 100  # cm^2
 old_system = System(
     die_size=old_die_size,
-    TDP=old_TDP,
+    TDP=None,
     specint=1,
-    lifetime=5,
+    lifetime=10,
     dram_capacity=8 * 64,
     ssd_capacity=2 * 1600,
     hdd_capacity=0
