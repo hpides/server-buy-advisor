d8e97b7d949260e86a1e892ce5b7bc8713aa04c9
Martin Boissier <martin.boissier@hpi.de>
2024-08-02 00:02:42 +0200

    Plotting

---
+++ measured-results/plot__sorting_and_tpch.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv("sorting_and_tpch.csv", decimal=",")
+df = df.replace({"Intel Xeon ": "", "Gold ": "", "Platinum ": ""}, regex=True)
+
+df["PLOT_KEY"] = df.LAUNCH_YEAR.astype(str) + " Q" + df.LAUNCH_QUARTER.astype(str) + " (" + df.CPU_IDENTIFIER + ")"
+
+df["K_SORTED_TUPLES_PER_S"] = df["SORTED_TUPLES_PER_S"] / 1_000
+df["M_SORTED_TUPLES_PER_S"] = df["SORTED_TUPLES_PER_S"] / 1_000_000
+
+df_melted = pd.melt(df, id_vars=["PLOT_KEY"], value_vars=["SORTED_TUPLES_PER_S", "TPCH_RUNS_PER_H", "SPECINT_RATE"], var_name="BENCHMARK")
+
+# facet_plot = sns.catplot(data=df_melted, kind="bar", col="BENCHMARK", x="PLOT_KEY", y="value", sharex=False, sharey=False,  hue="PLOT_KEY")
+# facet_plot.set_xticklabels(rotation=70)
+# facet_plot.set(ylim=(0, None))
+
+# fig = facet_plot.fig
+# fig.savefig("sorting_and_tpch.pdf") 
+
+# plt.show()
+# plt.clf()
+
+
+# fig = plt.figure(layout="constrained", figsize=(1, 4))
+# subfigs = fig.subfigures(1, 3, wspace=0.07)
+
+#set seaborn plotting aesthetics as default
+sns.set()
+#define plotting region (2 rows, 2 columns)
+fig, axes = plt.subplots(1, 3, figsize=(14, 2.25), sharey=True)
+
+for axis_id, column_name, axis_title, plot_title in [(0, "SPECINT_RATE", "Result score", "SPECrate 2017 Integer"),
+                                                     (1, "TPCH_RUNS_PER_H", "Runs per hour", "TPC-H - High Load"),
+                                                     (2, "K_SORTED_TUPLES_PER_S", "Thousand tuples sorted per second", "Parallel std::sort")]:
+  sns.barplot(data=df, x=column_name, y="PLOT_KEY", ax=axes[axis_id], hue="PLOT_KEY", orient="h")
+  axes[axis_id].set_title(plot_title, weight="bold")
+  axes[axis_id].set_xlabel(axis_title)
+  axes[axis_id].set_ylabel(None)
+  axes[axis_id].text(df[column_name].min() + (df[column_name].max() - df[column_name].min()) / 2, 0.075,
+                     f"{df[column_name].max() / df[column_name].min():.2f}x", horizontalalignment="center",
+                     color="#404040", fontsize=10)
+  axes[axis_id].annotate('', xy=(df[column_name].min(), 0.2), xytext=(df[column_name].max(), 0.2), 
+                         arrowprops=dict(arrowstyle="<->", color="#404040"))
+
+# plt.yticks(va="bottom")
+plt.tight_layout()
+fig.savefig("sorting_and_tpch2.pdf")
